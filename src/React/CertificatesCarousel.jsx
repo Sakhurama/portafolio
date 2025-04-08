fbc5d3a18972a89202e0df0a854c489349749ad1
Daniel Casita <legendadtwo@gmail.com>
2025-04-08 17:28:48 -0500

    Fix: cambio Class a ClassName en React y suprimo rutas relativas de imagenes hacia public

--- src/React/CertificatesCarousel.jsx
+++ src/React/CertificatesCarousel.jsx
@@ -8,43 +8,43 @@ import 'swiper/css/pagination';
 const certificates = [
     {
         title: "Fundamentos Javascript",
-        img: "../../public/certificados/fundamentos-javascript.jpg",
+        img: "/certificados/fundamentos-javascript.jpg",
         link: "https://platzi.com/p/danimitad/curso/10266-javascript/diploma/detalle/",
         description: "Platzi",
     },
     {
         title: "Bases de Datos SQL",
-        img: "../../public/certificados/sql.jpg",
+        img: "/certificados/sql.jpg",
         link: "https://www.udemy.com/certificate/UC-7f4bca64-4cf6-4df9-81c4-261a43c3b40d/",
         description: "Udemy",
     },
     {
         title: "Fundamentos API REST",
-        img: "../../public/certificados/api.jpg",
+        img: "/certificados/api.jpg",
         link: "https://platzi.com/p/danimitad/curso/2985-api/diploma/detalle/",
         description: "Platzi",
     },
     {
         title: "Curso de WordPress",
-        img: "../../public/certificados/wordpress.jpg",
+        img: "/certificados/wordpress.jpg",
         link: "https://platzi.com/p/danimitad/curso/4194-paginas-web/diploma/detalle/",
         description: "Platzi",
     },
     {
         title: "Diseño de Interfaces",
-        img: "../../public/certificados/ui-ux.jpg",
+        img: "/certificados/ui-ux.jpg",
         link: "https://platzi.com/p/danimitad/curso/5184-basico-ui/diploma/detalle/",
         description: "Platzi",
     },
     {
         title: "Curso de Git y GitHub",
-        img: "../../public/certificados/git.jpg",
+        img: "/certificados/git.jpg",
         link: "https://platzi.com/p/danimitad/curso/1557-git-github/diploma/detalle/",
         description: "Platzi",
     },
     {
         title: "Frontend Developer",
-        img: "../../public/certificados/frontend.jpg",
+        img: "/certificados/frontend.jpg",
         link: "https://platzi.com/p/danimitad/curso/2467-frontend-developer/diploma/detalle/",
         description: "Platzi",
     },
@@ -73,26 +73,26 @@ export default function CertificatesCarousel() {
         >
             {certificates.map((cert, index) => (
             <SwiperSlide key={index}>
-                <div class="group">
+                <div className="group">
                     <a
                     href={cert.link}
                     target="_blank"
                     rel="noopener noreferrer"
-                    class="block"
+                    className="block"
                     >
-                    <div class="rounded-2xl overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 mb-4">
+                    <div className="rounded-2xl overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 mb-4">
                         <img
                         src={cert.img}
                         alt={cert.title}
-                        class="w-full h-48 md:h-64 object-cover group-hover:scale-105 transition-transform duration-300"
+                        className="w-full h-48 md:h-64 object-cover group-hover:scale-105 transition-transform duration-300"
                         />
                     </div>
-                    <div class="flex items-center px-3">
-                        <div class="flex-grow">
-                            <h4 class="text-2xl font-semibold text-[var(--white)]">
+                    <div className="flex items-center px-3">
+                        <div className="flex-grow">
+                            <h4 className="text-2xl font-semibold text-[var(--white)]">
                                 {cert.title}
                             </h4>
-                            <span class="py-1 text-sm text-[var(--white-icon)]">
+                            <span className="py-1 text-sm text-[var(--white-icon)]">
                                 {cert.description}
                             </span>
                         </div>
